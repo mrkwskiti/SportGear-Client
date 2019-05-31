89a8ba7cc70b36940d31cb87b89564f13358dddc
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-01 06:08:06 +0700

    Update fetching user

--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -4,7 +4,6 @@
       <div class="field">
         <h2 class="title is-1">Sign Up</h2>
       </div>
-
       <div class="columns">
         <div class="column">
           <label class="label">SID</label>
@@ -19,7 +18,7 @@
         <div class="column is-narrow">
           <label class="label">University</label>
           <div class="select">
-            <select v-model="user.university">
+            <select v-model="user.university" :disabled="user.loaded">
               <option>Chiang mai university</option>
               <option>Chulalongkorn university</option>
               <option>Thammasat university</option>
@@ -31,18 +30,33 @@
       <div class="columns">
         <div class="column">
           <label for="" class="label">First Name</label>
-          <input v-model="user.firstName" type="text" class="input" />
+          <input
+            v-model="user.firstName"
+            type="text"
+            class="input"
+            :disabled="user.loaded"
+          />
         </div>
         <div class="column">
           <label for="" class="label">Last Name</label>
-          <input v-model="user.lastName" type="text" class="input" />
+          <input
+            v-model="user.lastName"
+            type="text"
+            class="input"
+            :disabled="user.loaded"
+          />
         </div>
       </div>
 
       <div class="columns">
         <div class="column">
           <label class="label">Email</label>
-          <input v-model="user.email" type="text" class="input" />
+          <input
+            v-model="user.email"
+            type="text"
+            class="input"
+            :disabled="user.loaded"
+          />
         </div>
       </div>
     </div>
@@ -55,10 +69,34 @@ export default {
     return {
       user: {
         sid: '',
-        university: '',
         firstName: '',
         lastName: '',
-        email: ''
+        university: '',
+        email: '',
+        loaded: false
+      }
+    }
+  },
+  watch: {
+    'user.sid': function(val) {
+      this.fetchUser(val)
+    }
+  },
+  methods: {
+    async fetchUser(sid) {
+      if (this.user.sid.length === 13) {
+        const response = await this.$axios.$get(
+          'http://localhost:3000/users?sid=' + sid
+        )
+        console.log(response)
+        console.log(response[0].firstName)
+        this.user.firstName = response[0].firstName
+        this.user.lastName = response[0].lastName
+        this.user.university = response[0].university
+        this.user.email = response[0].email
+        this.user.loaded = true
+      } else {
+        this.user.loaded = false
       }
     }
   }
