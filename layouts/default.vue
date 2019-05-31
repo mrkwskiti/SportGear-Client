bb3138d291b4b5a4ecc45450512398bfceb11c64
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-01 03:44:30 +0700

    Update navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,21 +5,11 @@
       role="navigation"
       aria-label="main navigation"
     >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
+      <div class="navbar-brand"></div>
     </nav>
 
     <section class="main-content columns">
-      <aside class="column is-2 section">
+      <!-- <aside class="column is-2 section">
         <p class="menu-label is-hidden-touch">General</p>
         <ul class="menu-list">
           <li v-for="(item, key) of items" :key="key">
@@ -28,7 +18,7 @@
             </nuxt-link>
           </li>
         </ul>
-      </aside>
+      </aside> -->
 
       <div class="container column is-10">
         <nuxt />
