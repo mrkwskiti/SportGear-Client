1affbe40efd0872f30c12fca251448cff178b3ae
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-09 17:42:20 +0700

    Refactoring code to buefy syntax

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,34 +1,22 @@
 <template>
   <div :class="{ 'box hero is-light': isRegister }">
-    <div class="field">
-      <p class="control has-icons-right">
-        <input v-model="sid" class="input" type="text" placeholder="sid" />
-        <span class="icon is-small is-right">
-          <b-icon icon="check"></b-icon>
-        </span>
-      </p>
-      <p v-if="!isRegister" class="help">
-        {{ fullName }}
-      </p>
-    </div>
-    <div v-if="isRegister">
-      <div class="field is-grouped">
-        <input
-          type="text"
-          class="input control is-expanded"
-          placeholder="First Name"
-        />
-        <input
-          type="text"
-          class="input control is-expanded"
-          placeholder="Last Name"
-        />
-      </div>
-      <div class="field">
-        <div class="control">
-          <input type="text" class="input" placeholder="E-mail" />
-        </div>
-      </div>
+    <b-field :message="[!isRegister ? fullName : null]">
+      <b-input v-model="sid" expanded placeholder="SID"></b-input>
+    </b-field>
+
+    <section v-if="isRegister">
+      <b-field grouped>
+        <b-input expanded placeholder="First Name"></b-input>
+        <b-input expanded placeholder="Last Name"></b-input>
+        <b-select v-if="isRegister" placeholder="Gender">
+          <option>Male</option>
+          <option>Female</option>
+        </b-select>
+      </b-field>
+
+      <b-field>
+        <b-input placeholder="Email"></b-input>
+      </b-field>
       <div class="field is-grouped is-grouped-right">
         <div class="control">
           <a class="button is-primary is-right">
@@ -36,7 +24,7 @@
           </a>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
