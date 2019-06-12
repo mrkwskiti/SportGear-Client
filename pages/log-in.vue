287e953eeef06a64b5de00e25d90aef39d8ed1a0
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-12 08:00:53 +0700

    Update template login

--- pages/log-in.vue
+++ pages/log-in.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <h1>Log in</h1>
+  <div class="container grid">
+    <h2 class="title">Log in</h2>
+    <div class="columns">
+      <div class="column">
+        <b-input v-model="user" placeholder="User name"></b-input>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column">
+        <b-input
+          v-model="password"
+          type="password"
+          placeholder="Password"
+        ></b-input>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column">
+        <b-field grouped position="is-centered">
+          <b-button class="is-primary">Login</b-button>
+        </b-field>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +38,19 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+h2 {
+  padding: 60px 0 0 0;
+}
+.grid {
+  display: grid;
+  grid-template-columns: [xl-start] 1fr 1.5rem [md-start] minmax(0, 259px) [md-end] 1.5rem 1fr [xl-end];
+}
+
+.grid * {
+  grid-column: md;
+}
+.grid-xl {
+  grid-column: xl;
+}
+</style>
