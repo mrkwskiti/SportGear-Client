b6ee39d25bc4ce5359f3fee21aba4922fd05fbd2
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-12 07:45:08 +0700

    Add log in page

---
+++ pages/log-in.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Log in</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style></style>
