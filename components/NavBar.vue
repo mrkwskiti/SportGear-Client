38270ffa4397e1856d9f0509c0cdbcf9a9af64bb
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-15 12:58:06 +0700

    Update navbar template (band, univer label)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,79 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar is-primary"
       role="navigation"
-      aria-label="navbar-band"
+      aria-label="main navigation"
     >
-      <div class="navbar-brand"></div>
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <p class="subtitle is-3 has-text-white">Sport Gear</p>
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <!-- menu bar -->
+        <!-- <div class="navbar-start">
+          <a class="navbar-item">
+            Home
+          </a>
+
+          <a class="navbar-item">
+            Documentation
+          </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <b-icon id="univer-icon" pack="fas" icon="university"></b-icon>
+            <p>Chiang mai University</p>
+          </div>
+        </div>
+      </div>
     </nav>
   </div>
 </template>
+
+<style>
+nav {
+  padding: 20px;
+}
+
+#univer-icon {
+  padding: 17px 20px 20px 20px;
+}
+</style>
