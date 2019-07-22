1d42613461e7e188b4cea1618d4520451d76fe7a
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-07-23 04:53:14 +0700

    Add init UsersTable component

---
+++ components/UsersTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <hot-table
+      class="hot handsontable htColumnHeaders"
+      :settings="hotSettings"
+    ></hot-table>
+  </div>
+</template>
+
+<script>
+import HotTable from '~/plugins/vue-handsontable'
+export default {
+  components: {
+    HotTable
+  },
+  data() {
+    return {
+      hotSettings: {
+        startRows: 300,
+        startCols: 4,
+        colHeaders: ['SID', 'First Name', 'Last Name', 'E-mail'],
+        rowHeaders: true,
+        stretchH: 'all',
+        height: '340',
+        manualColumnMove: true,
+        licenseKey: 'non-commercial-and-evaluation'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
