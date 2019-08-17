14b87b2fad23b9da96c9b37c4875e5d87e6c5086
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-08-17 21:11:12 +0700

    Move api service to server of client side

--- services/AuthService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default {
-  setToken(token) {
-    axios.defaults.headers.common.Authorization = token
-    return axios.post('/session/token').then(res => res.data)
-  }
-}
