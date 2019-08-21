92bb84eaf6146c96100cb0644452c786a533aa3c
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-08-21 07:18:18 +0700

    Add lang file

---
+++ lang/en-US.js
@@ -0,0 +1,8 @@
+export default {
+  index: {
+    welcome: 'Jhanglek Games',
+    date: '23-27 December 2019',
+    where: 'Faculty of Engineering, Chiang mai University',
+    registerButton: 'Register athletes/teams'
+  }
+}
