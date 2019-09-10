8245da6dec31bd500bfbd98562c1db7ff8fbd091
Thanatossan <kotokikung@gmail.com>
2019-09-11 00:08:03 +0700

    add change password template

--- pages/change-password.vue
+++ pages/change-password.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-    <p>change password</p>
+  <div class="container grid">
+    <div class="columns">
+      <div class="column has-text-centered">
+        <p class="title">Change password</p>
+        <div class="columns">
+          <div class="column">
+            <section>
+              <b-field>
+                <!-- have to add v-model to password-->
+                <b-input placeholder="old password" type="password"> </b-input>
+              </b-field>
+              <b-field>
+                <!-- have to add v-model to password-->
+                <b-input
+                  placeholder="new password"
+                  type="new password"
+                ></b-input
+              ></b-field>
+              <b-button class="is-primary is-fullwidth">
+                Submit new password
+              </b-button>
+              <b-button
+                class="is-fullwidth"
+                rounded
+                type="is-light"
+                style="margin-top : 10px"
+                tag="router-link"
+                to="/sign-up"
+              >
+                Cancle
+              </b-button>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +42,8 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.title {
+  margin-top: 60px;
+}
+</style>
